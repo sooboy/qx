f2c895a2fd4c002bbdd24f6df682a4462cfa1d19
Regan <sooboy@foxmail.com>
2025-05-22 09:06:55 +0800

    feat: 添加ygzs.js脚本及result.json文件
    添加ygzs.js脚本用于处理特定URL的响应，并生成包含状态、有效期等信息的JSON数据。同时，添加result.json文件以存储生成的JSON数据，便于后续使用和验证。

---
+++ ygzs.js
@@ -0,0 +1,19 @@
+/******************************
+
+*******************************
+[rewrite_local]
+^http:\/\/xgpiao\.net:6789\/GetExpriteDateSVIP?* url script-response-body https://raw.githubusercontent.com/sooboy/qx/main/ygzs.js
+^http:\/\/xgpiao\.net:6693\/login?* url script-response-body https://raw.githubusercontent.com/sooboy/qx/main/ygzs.js
+^http:\/\/xgpiao\.net:6694\/expritedate?* url script-response-body https://raw.githubusercontent.com/sooboy/qx/main/ygzs.js
+[mitm] 
+hostname = xgpiao.net
+*******************************/
+$done({body: JSON.stringify({
+    "ret_status": 1,
+    "ret_valide": 1,
+    "ret_regtime": "2020-12-02 00:04:53",
+    "ret_expritedtime": "2999-08-16 17:15:48",
+    "sjs_require_avalibe":1,
+    "ret_account":1,
+    "ret_note": "还可以使用 大约 9000 个月"
+})});
